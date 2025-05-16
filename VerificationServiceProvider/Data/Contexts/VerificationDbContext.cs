f3f37d785ddb6b906ce997d73eee5e7c1ee4a1d4
Jessica Campbell <jesscampb91@gmail.com>
2025-05-16 16:00:30 +0200

    Created VerificationDbContext

---
+++ VerificationServiceProvider/Data/Contexts/VerificationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VerificationServiceProvider.Data.Contexts;
+
+public class VerificationDbContext(DbContextOptions<VerificationDbContext> options) : DbContext(options)
+{
+    
+}
