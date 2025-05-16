34e4c255c6de4d8e374966de55b180f4af51f0c0
Jessica Campbell <jesscampb91@gmail.com>
2025-05-16 15:42:16 +0200

    Created GenerateVerificationCode in VerificationService.

---
+++ VerificationServiceProvider/Services/VerificationService.cs
@@ -0,0 +1,20 @@
+﻿using VerificationServiceProvider.Models;
+
+namespace VerificationServiceProvider.Services;
+
+public class VerificationService
+{
+    private static readonly Random _random = new();
+
+    public static VerificationCode GenerateVerificationCode(string email, int expiresInMinutes = 5)
+    {
+        var verificationCode = new VerificationCode
+        {
+            Email = email,
+            Code = _random.Next(100000, 999999).ToString(),
+            ExpiresAt = DateTime.UtcNow.AddMinutes(expiresInMinutes)
+        };
+
+        return verificationCode;
+    }
+}
