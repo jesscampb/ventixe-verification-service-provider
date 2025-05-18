a1dca3e772651d9d913636b50fb1d200e432c4f9
Jessica Campbell <jesscampb91@gmail.com>
2025-05-18 23:06:40 +0200

    Added functionality to function GenerateVerificationCode.

--- VerificationServiceProvider/Functions/GenerateVerificationCode.cs
+++ VerificationServiceProvider/Functions/GenerateVerificationCode.cs
@@ -1,31 +1,42 @@
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using VerificationServiceProvider.Services;
 
 namespace VerificationServiceProvider.Functions;
 
-public class GenerateVerificationCode
+public class GenerateVerificationCode(ILogger<GenerateVerificationCode> logger, VerificationService verificationService)
 {
-    private readonly ILogger<GenerateVerificationCode> _logger;
-
-    public GenerateVerificationCode(ILogger<GenerateVerificationCode> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<GenerateVerificationCode> _logger = logger;
+    private readonly VerificationService _verificationService = verificationService;
 
     [Function(nameof(GenerateVerificationCode))]
-    public async Task Run(
+    public async Task<string?> Run(
         [ServiceBusTrigger("verification-code-requested", Connection = "ServiceBusConnection")]
         ServiceBusReceivedMessage message,
         ServiceBusMessageActions messageActions)
     {
-        _logger.LogInformation("Message ID: {id}", message.MessageId);
-        _logger.LogInformation("Message Body: {body}", message.Body);
-        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+        var email = message.Body?.ToString();
+        if (!string.IsNullOrEmpty(email))
+        {
+            var emailRequest = await _verificationService.GenerateVerificationCodeEmailAsync(email);
+            if (emailRequest != null)
+            {
+                await messageActions.CompleteMessageAsync(message);
+                _logger.LogInformation("Verification code was successfully sent.");
 
-        // Complete the message
-        await messageActions.CompleteMessageAsync(message);
+                return JsonSerializer.Serialize(emailRequest);
+            }
+        }
+
+        _logger.LogWarning("Received null or empty email from message.");
+        await messageActions.DeadLetterMessageAsync(message, new Dictionary<string, object> {
+            { "Reason", "Email address is null or empty." }
+        });
+
+        return null;
     }
 }
