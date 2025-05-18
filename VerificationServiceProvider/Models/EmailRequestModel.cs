56b47356780eb6837b552b82bf5b30a767f1f753
Jessica Campbell <jesscampb91@gmail.com>
2025-05-18 17:45:10 +0200

    Created model for email requests.

---
+++ VerificationServiceProvider/Models/EmailRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VerificationServiceProvider.Models;
+
+public class EmailRequestModel
+{
+    public List<string> Recipients { get; set; } = new();
+    public string Subject { get; set; } = "No Subject";
+    public string PlainText { get; set; } = string.Empty;
+    public string Html { get; set; } = string.Empty;
+}
